63f482ad6e36b58d4669d46ea18a3a267f438bbd
opgohil1 <65224148+opgohil1@users.noreply.github.com>
2020-05-12 15:33:35 +0530

    Update zombies.py

--- userbot/plugins/zombies.py
+++ userbot/plugins/zombies.py
@@ -5,7 +5,7 @@
 """Cmd= `.zombie`
 Usage: Searches for deleted accounts in a groups and channels.
 Use .zombies clean to remove deleted accounts from the groups and channels.
-\nPorted by ©[NIKITA](t.me/kirito6969) and ©[EYEPATCH](t.me/NeoMatrix90)"""
+\nPorted by ©[NIKITA](t.me/kirito6969) and ©[OPGOHIL](t.me/OPGOHIL)"""
 
 from telethon import events
 from userbot.utils import admin_cmd
